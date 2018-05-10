b6166f7c860cc6d1150ef4f61b83ece1521a42b7
Vini <viniciusalves@ice.ufjf.br>
2018-05-10 14:24:26 -0300

    ultimas mudancas

---
+++ Requisitolândia/Assets/Scripts/NameLoad.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameLoad : MonoBehaviour {
+    private Text usrName;
+    private Text usrTime;
+    private string nome;
+    DateTime startTime;
+    private string[] line;
+    // Use this for initialization
+    void Start () {
+        startTime = DateTime.Now;
+        usrName = GameObject.Find("Text").GetComponent<Text>();
+        usrTime = GameObject.Find("tempoDecorrido").GetComponent<Text>();
+        line = File.ReadAllLines(GetPathToList());
+        usrName.text = line[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        string time = GetElapsedTime().TotalSeconds.ToString();
+        usrTime.text = time;
+        print(usrName.text);
+        print(int.Parse(GetElapsedTime().Minutes.ToString()));
+    }
+
+    public TimeSpan GetElapsedTime()
+    {
+        TimeSpan elapsedTime = DateTime.Now - startTime;
+        
+        return elapsedTime;
+    }
+
+    public string GetPathToList()
+    {
+        string path = null;
+        try
+        {
+            // Caminho para arquivo contendo o nome de todas as fichas adcionadas a "sessão"
+            return "Assets/Resources/Atual.txt";
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+        return path;
+
+    }
+
+    public void WriteCharacterName(string name)
+    {
+        //Escreve o nome de um personagem na lista contendo os personagens de uma seção
+        string path = GetPathToList();
+        File.WriteAllText("Assets/Resources/Atual.txt", name + Environment.NewLine);
+
+    }
+
+    public void Voltar()
+    {
+        File.AppendAllText(GetPathToList(), GetElapsedTime().TotalSeconds.ToString());
+        SceneManager.LoadScene("nameInput");
+    }
+
+    public void Continuar()
+    {
+
+        SceneManager.LoadScene("Fase2");
+    }
+}
---
+++ Requisitolândia/Assets/Scripts/nomeUsuario.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class nomeUsuario : MonoBehaviour
+{
+    private Text textUm;
+    private GameObject usuario;
+    private InputField input;
+    private string usrName;
+    private loadAndReadTxt script;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        script = GameObject.Find("Controller").GetComponent<loadAndReadTxt>();
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+
+    }
+
+   public void Continuar()
+    {
+        print("alo");
+        usrName = input.text;
+        script.WriteCharacterName(usrName);
+        print(usrName);
+        SceneManager.LoadScene("Scenes/Fase1");
+    }
+
+    public void Voltar()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
