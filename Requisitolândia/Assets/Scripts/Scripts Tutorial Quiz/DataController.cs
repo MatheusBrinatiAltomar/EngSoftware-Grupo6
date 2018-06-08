4a0f12004c7eee3d6ed9262aef0240ee980829e2
Maria Luiza Ferreira Magalhães <marialuizaferreira88@gmail.com>
2018-06-07 22:18:59 -0300

    agora vai

---
+++ Requisitolândia/Assets/Scripts/Scripts Tutorial Quiz/DataController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.IO;                                                        // The System.IO namespace contains functions related to loading and saving files
+
+public class DataController : MonoBehaviour 
+{
+    private RoundData[] allRoundData;
+    private PlayerProgress playerProgress;
+    private int usrHighScore;
+
+    private string gameDataFileName = "data.json";
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        LoadGameData();
+
+        LoadPlayerProgress();
+
+        SceneManager.LoadScene("Scenes/MenuScreen");
+    }
+        
+    public RoundData GetCurrentRoundData()
+    {
+        // If we wanted to return different rounds, we could do that here
+        // We could store an int representing the current round index in PlayerProgress
+
+        return allRoundData[0];
+    }
+
+    public void SubmitNewPlayerScore(int newScore)
+    {
+        // If newScore is greater than playerProgress.highestScore, update playerProgress with the new value and call SavePlayerProgress()
+        if(newScore > usrHighScore)
+        {
+            usrHighScore = newScore;
+            SavePlayerProgress();
+        }
+    }
+
+    public int GetHighestPlayerScore()
+    {
+        return usrHighScore;
+    }
+
+    private void LoadGameData()
+    {
+        // Path.Combine combines strings into a file path
+        // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if(File.Exists(filePath))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(filePath); 
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            // Retrieve the allRoundData property of loadedData
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    // This function could be extended easily to handle any additional data we wanted to store in our PlayerProgress object
+    private void LoadPlayerProgress()
+    {
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            usrHighScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
+
+
+    // This function could be extended easily to handle any additional data we wanted to store in our PlayerProgress object
+    private void SavePlayerProgress()
+    {
+        // Save the value playerProgress.highestScore to PlayerPrefs, with a key of "highestScore"
+        PlayerPrefs.SetInt("highestScore", usrHighScore);
+    }
+}
---
+++ Requisitolândia/Assets/Scripts/Scripts Tutorial Quiz/PlayerProgress.cs
@@ -0,0 +1,17 @@
+﻿public class PlayerProgress
+{
+    private int highestScore = 0;
+
+    public int HighestScore
+    {
+        get
+        {
+            return highestScore;
+        }
+
+        set
+        {
+            highestScore = value;
+        }
+    }
+}
